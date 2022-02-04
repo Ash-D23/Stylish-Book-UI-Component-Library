fe3ca1c14e02f6f160c762b702ed4f21fcd270c3
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-02-05 00:44:22 +0530

    bug: fixed initial load js script problem

--- Documentation/document.js
+++ Documentation/document.js
@@ -1,20 +1,15 @@
-let doc__menu = document.querySelector('.navbar__menu');
-let doc__icon__menu = document.querySelector('.icon__menu');
-let doc__icon__times = document.querySelector('.icon__times');
-let doc__drawer = document.querySelector('.doc__drawer');
-let navbar__list = document.querySelector('.navbar__list-container')
-
-if(doc__menu){
-    doc__menu.addEventListener('click', ()=>{
-        doc__icon__menu.classList.toggle('hide');
-        doc__icon__times.classList.toggle('hide');
-        doc__drawer.classList.toggle('doc__drawer--display');
-        navbar__list.classList.toggle('navbar__display');
-    })
-}
-
 let moon = document.querySelector('.moon');
 let sun = document.querySelector('.sun');
+
+let theme = localStorage.getItem("datatheme");
+console.log(theme)
+if(theme === 'dark'){
+    document.body.setAttribute('data-theme', 'dark');
+    sun.classList.remove('hide')
+}else{
+    document.body.setAttribute('data-theme', 'light');
+    moon.classList.remove('hide')
+}
 
 if(moon){
     moon.addEventListener('click', ()=>{
@@ -36,16 +31,20 @@ if(sun){
     })
 }
 
-window.onload = function(){
-    let theme = localStorage.getItem("datatheme");
-    console.log(theme)
-    if(theme === 'dark'){
-        document.body.setAttribute('data-theme', 'dark');
-        sun.classList.remove('hide')
-    }else{
-        document.body.setAttribute('data-theme', 'light');
-        moon.classList.remove('hide')
-    }
+let doc__menu = document.querySelector('.navbar__menu');
+let doc__icon__menu = document.querySelector('.icon__menu');
+let doc__icon__times = document.querySelector('.icon__times');
+let doc__drawer = document.querySelector('.doc__drawer');
+let navbar__list = document.querySelector('.navbar__list-container')
+
+if(doc__menu){
+    doc__menu.addEventListener('click', ()=>{
+        doc__icon__menu.classList.toggle('hide');
+        doc__icon__times.classList.toggle('hide');
+        doc__drawer.classList.toggle('doc__drawer--display');
+        navbar__list.classList.toggle('navbar__display');
+    })
 }
+
 
 
--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # Stylish Book UI: Component-Library
+
+### Demo Link: 
+
+https://stylishbookui.netlify.app/
+
+## Introduction
+
+Stylish book UI is a Component library which is created with CSS, JavaScript and HTML. Where you can easily setup CSS Components and Style for your Web Project in few minutes. It Consists of 10+ CSS Components which you can easily copy paste onto your web project by checking the [documentation](https://stylishbookui.netlify.app/documentation/documentation) for code snippets.
+
+
+## How To Use
+
+Add the following link into the head tag of the Page 
+
+```
+<link rel="stylesheet" href="https://stylishbookui.netlify.app/Components/stylishbook.css" />
+```
+
+To add js functionality add the following code at the end of body
+
+```
+<script src="https://stylishbookui.netlify.app/Components/JS/script.js"></script>
+```
+
+The Component Library Comes in both Dark and Light theme to change theme set data-theme attribute to dark or light in body
+
+
+## Features
+
+Following Components are available in the Component Library
+
+```
+Avatar
+Alert
+Badge
+Button
+Card
+Image
+Form & Input
+Text Utilities
+Lists
+Navigation
+Modal
+Rating 
+Toast
+Simplified Grid
+Navigation
+Drawer
+```
